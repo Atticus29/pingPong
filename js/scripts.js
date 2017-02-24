e989aca35c85ff8958cdd7fe522ca68ee5657fb2
Mark <mark.aaron.fisher@gmail.com>
2017-02-24 09:03:14 -0800

    add a validates function that has been thoroughly tested to satisfy spec 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,27 @@
 // Back End
+var processInput = function(str){
+  if(validates(str)){
+    // console.log("this happened");
+  }else{
+    alert("Please enter a positive integer");
+    return false;
+  }
+}
+var validates = function(str){
+  if(parseInt(str) && parseInt(str)>0 && Math.trunc(str)===parseFloat(str)){
+    return true;
+  } else{
+    return false;
+  }
+}
+
 
 // Front End
 $(function(){
-
+  $("#submitForm").submit(function(){
+    event.preventDefault();
+    var usrStr = $("#usrInput").val();
+    processInput(usrStr);
+    // console.log(usrStr);
+  });
 });
