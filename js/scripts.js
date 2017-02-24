8c38cef719e78f69fa6b17f87e890562c66dcc60
Mark <mark.aaron.fisher@gmail.com>
2017-02-24 09:18:35 -0800

    satisfy specs. 4, 5, 6, and 7 with the addition of the changeValuesInIndecesDivisibleByXtoY function and by calling it three times in the wrapper function processInput

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,22 @@
 // Back End
 var processInput = function(str){
+  var returnVal = undefined;
   if(validates(str)){
     num = parseInt(str);
     var numArray = constructArray(num);
+    numArray = changeValuesInIndecesDivisibleByXtoY(3, "ping", numArray);
+    // console.log("numArray is " + numArray);
+    numArray = changeValuesInIndecesDivisibleByXtoY(5, "pong", numArray);
+    // console.log("numArray is " + numArray);
+    numArray = changeValuesInIndecesDivisibleByXtoY(15, "ping-pong", numArray);
     console.log("numArray is " + numArray);
+    returnVal = numArray;
+
   }else{
     alert("Please enter a positive integer");
-    return false;
+    returnVal = undefined;
   }
+  return returnVal;
 }
 var validates = function(str){
   if(parseInt(str) && parseInt(str)>0 && Math.trunc(str)===parseFloat(str)){
@@ -20,6 +29,16 @@ var constructArray = function(num){
   var numArray = [];
   for (var i = 0; i<num; i++){
     numArray.push(i+1);
+  }
+  return numArray;
+}
+var changeValuesInIndecesDivisibleByXtoY = function(x, y, numArray){
+  for(var i = 0; i<numArray.length; i++){
+    if((i+1)%x===0){
+      numArray[i] = y;
+    }else{
+      console.log(i+1 + " is not divisible by " + x);
+    }
   }
   return numArray;
 }
